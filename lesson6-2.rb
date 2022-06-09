928aedb16c8bf96d55666fa691e315f49c9b5d70
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-06-09 10:28:41 +0000

    2-6[finish]

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -1,14 +1,24 @@
 puts "計算をはじめます"
 puts "何回計算を繰り返しますか？"
 i = gets.to_i
-while i >= 1 do
-  puts "s回目の計算"
-　puts "2つの値を入力してください"
-　a = gets.to_i
-　b = gets.to_i
-　puts "計算結果を出力します"
-　puts "a+b=#{a+b}"
-　puts "a-b=#{a-b}"
-　puts "a*b=#{a*b}"
-　puts "a/b=#{a/b}"
+s = 1
+while i >= s do
+  puts "#{s}回目の計算"
+  puts "2つの値を入力してください"
+  
+  a = gets.to_i
+  b = gets.to_i
+  
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "a+b=#{a+b}"
+  puts "a-b=#{a-b}"
+  puts "a*b=#{a*b}"
+  puts "a/b=#{a/b}"
+  
+  s += 1
+  
 end
+
+puts "計算を終了します"
