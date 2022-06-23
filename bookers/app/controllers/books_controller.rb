26841b096ccdd90eb39ef37993dcf6930389c2e4
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-06-23 13:15:44 +0000

    20220623

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -6,13 +6,17 @@ class BooksController < ApplicationController
     @books = Book.new
     @lists = Book.all
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @lists = Book.all
+      render :index
+    end
   end
-    
+
 
   def show
     @book = Book.find(params[:id])
@@ -21,22 +25,26 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      @book = Book.find(params[:id])
+      render :show
+    end
   end
-  
+
   def destroy
    book = Book.find(params[:id])
-   book.destroy(book_params)
+   book.destroy
    redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
