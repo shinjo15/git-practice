397329f016c045095c6483a9fbac0011a0cbc488
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-06-25 12:31:29 +0000

    20220625-2

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/top'=>'homes#top'
+  root :to=>'homes#top'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
