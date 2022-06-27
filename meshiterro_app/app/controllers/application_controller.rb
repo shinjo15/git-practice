0f37e8eebcd7093ce6a3886d363ad673d1f457e2
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-06-27 22:32:54 +0000

    5-6

--- meshiterro_app/app/controllers/application_controller.rb
+++ meshiterro_app/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permtted_parmeitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permtted_parmeitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
