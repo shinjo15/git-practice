0066efc6e09fe255662c23a4bce62c490497ed42
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-07-11 11:57:38 +0000

    20220711

--- bookers2/app/models/book.rb
+++ bookers2/app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
 end
+  
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+  end
+end
