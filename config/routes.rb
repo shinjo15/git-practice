6c3db272ef24a265e9d356ebdbf16ea1fbe2b74a
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-02 12:14:31 +0000

    create db

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,29 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  root to: 'public#homes#top'
+  
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: "about"
+  
+  resources :items, only: [:index, :show]
+  resources :customers, only: [:show, :edit, :update]
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw'
+  resources :cart_items, only: [:index, :update, :create, :destroy]
+  delete 'customers/destroy_all' => 'customers#destroy_all'
+  resources :orders, only: [:new, :create, :index, :show]
+  post 'customers/confirm' => 'customers#confirm'
+  get 'customers/complete' => 'customers#complete'
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  
+  namespace :admin do
+    get 'homes/top' => 'homes#top', as: "/admin"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [ :show, :update]
+    resources :order_details, only: [:update]
+    end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20220801122734_create_order_items.rb
+++ db/migrate/20220801122734_create_order_items.rb
@@ -2,10 +2,10 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
       t.integer :order_id, null: false
-      integer :item_id, null: false
-      integer :purchase_price, null: false
-      integer :amount, null: false
-      integer :production_status, null: false
+      t.integer :item_id, null: false
+      t.integer :purchase_price, null: false
+      t.integer :amount, null: false
+      t.integer :production_status, null: false
 
       t.timestamps
     end
