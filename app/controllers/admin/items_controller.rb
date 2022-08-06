c5074830f1bc48746df4f60afaf27f8ff48f9d91
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-06 07:30:15 +0000

    Add Admin Item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,29 @@
 class Admin::ItemsController < ApplicationController
   def index
   end
-  
+
   def new
+    @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(item.id)
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
