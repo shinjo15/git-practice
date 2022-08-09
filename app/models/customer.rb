7e18e321efaa9d4098e5db82c37a556eacfa1d14
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-09 11:30:01 +0000

    UpdateModel

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :order
+  has_many :cart_item
+  has_many :address
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  has_many :cart_item
+  has_many :order_item
 end
