22e752435875b234591c5de525ac906626a86c81
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-13 12:49:22 +0000

    UpdateAdminTop

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_item
   belongs_to :customer
+  
+  enum order_status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, preparing_to_ship: 3, sent: 4 }
 end
