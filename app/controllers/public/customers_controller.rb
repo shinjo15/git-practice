4d09d361f07de8068fde1b626d9a2c69d471e3fd
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-17 00:02:32 +0000

    AddCustomerShow

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,7 @@
 class Public::CustomersController < ApplicationController
   def show
+    @user = current_customer
+    @customer = customer.find(params[:id])
   end
   
   def edit
@@ -13,4 +15,9 @@ class Public::CustomersController < ApplicationController
   
   def withdraw
   end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_active)
+  end
+  
 end
