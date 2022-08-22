37ca89ad6605ab3aab8e90155ea203b5e6739f09
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-22 16:00:44 +0000

    AddAddressIndex

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,11 +1,16 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address_new = Address.new
+    @address = Address.all
   end
   
   def edit
   end
   
   def create
+    @address_new = Address.new(address_params)
+    @address_new.save
+    redirect_to addresses_path
   end
   
   def update
@@ -13,4 +18,11 @@ class Public::AddressesController < ApplicationController
   
   def destroy
   end
+  
+   private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name )
+  end
+  
 end
