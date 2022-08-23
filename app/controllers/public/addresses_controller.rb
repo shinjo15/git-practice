03c23ae39f53e26ba3384c752484441254c31d28
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-08-23 12:18:13 +0000

    UpdateAddressIndex

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,28 +1,29 @@
 class Public::AddressesController < ApplicationController
   def index
-    @address_new = Address.new
-    @address = Address.all
+    @address = Address.new
+    @addresses = Address.all
   end
-  
+
   def edit
   end
-  
+
   def create
-    @address_new = Address.new(address_params)
-    @address_new.save
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
     redirect_to addresses_path
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
    private
 
   def address_params
     params.require(:address).permit(:postal_code, :address, :name )
   end
-  
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :cart_items
-  has_many :addreses
+  has_many :addresses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
