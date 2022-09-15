dd1d41d5e4e0f30cbff69bbd82549d0ef020004c
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-09-15 12:54:36 +0000

    UpdateCartitemsController

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -24,14 +24,22 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save!
+    @cart_items = current_customer.cart_items.all
+      if CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id )
+        @sam = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: current_customer.id )
+        new_amount = @sam.amount + @cart_item.amount
+        @sam.update(amount: new_amount)
+        @cart_item.delete
+      else
+        @cart_item.customer_id = current_customer.id
+        @cart_item.save!
+      end
     redirect_to cart_items_path
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
