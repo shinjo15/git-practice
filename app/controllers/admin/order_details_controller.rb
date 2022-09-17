abab844a9646ce171c4177db5669889f64a88a25
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-09-17 06:43:29 +0000

    UpdateOrderDetailsController

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-      @order_item = Order.find(params[:id])
-      @order_item.update
-      redirect_to admin_path(@order.id)
+      @order_item = OrderItem.find(params[:id])
+      @order_item.update(order_item_params)
+      redirect_to admin_path
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:production_status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
 
-devise_for :customers, controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, controllers: {
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   scope module: :public do
