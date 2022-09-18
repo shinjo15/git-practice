c66642d0f85ebfe3beacc6a695b3b35ead4b9ee1
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-09-18 12:36:41 +0000

    UpdateAdminOrdersShow

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,12 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+      @order = Order.find(params[:id])
       @order_item = OrderItem.find(params[:id])
+      if @order_item.production_status == 2
+        @order.update(order_status: "production")
+      elsif @order_item.production_status == 3
+        @order.update(order_status: "preparing_to_ship")
+      end
       @order_item.update(order_item_params)
       redirect_to admin_path
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,16 +3,16 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @name = @order.customer.last_name+@order.customer.first_name
   end
-  
+
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
-      redirect_to admin_path(@order.id)
-    else
-      render :edit
+    if @order.order_status == "payment_confirmation"
+      @order.order_items.update(production_status: 1)
     end
+    @order.update(order_params)
+    redirect_to admin_path(@order.id)
   end
-  
+
   private
 
   def order_params
